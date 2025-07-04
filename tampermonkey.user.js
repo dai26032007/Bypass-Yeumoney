20c3e96d1dfe92aa79927771d580e595be85a5e7
Đại <dai26032007@gmail.com>
2025-07-04 16:11:32 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Bypass Yeumoney (Tăng tốc)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Tăng tốc độ đếm giây, giảm thời gian đợi lấy mã xuống chỉ còn 1s
+// @author       Nguyễn Bá Đại
+// @require      https://raw.githubusercontent.com/dai26032007/Bypass-Yeumoney/refs/heads/main/bypass_yeumoney.js
+// @match        https://yeumoney.com/*
+// @match        *//*w88*/*
+// @match        *//*m88*/*
+// @match        *//*188bet*/*
+// @match        *//*bk8*/*
+// @match        *//*vn88*/*
+// @match        *//*fb88*/*
+// @exclude        https://yeumoney.com/quangly/check*
+// @grant        none
+// @icon         https://i.imgur.com/0l2I5mB.jpeg
+// ==/UserScript==
