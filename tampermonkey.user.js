bf42bd5470717d7d2274d3dd86913a4b483887dc
Đại <dai26032007@gmail.com>
2025-07-04 16:39:58 +0700

    Add files via upload

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -6,14 +6,14 @@
 // @author       Nguyễn Bá Đại
 // @require      https://raw.githubusercontent.com/dai26032007/Bypass-Yeumoney/refs/heads/main/bypass_yeumoney.js
 // @match        https://yeumoney.com/*
-// @match        *//*w88*/*
-// @match        *//*m88*/*
-// @match        *//*88bet*/*
-// @match        *//*bet88*/*
-// @match        *//*250*/*
-// @match        *//*bk8*/*
-// @match        *//*vn88*/*
-// @match        *//*fb88*/*
+// @match        *://*w88*/*
+// @match        *://*m88*/*
+// @match        *://*88bet*/*
+// @match        *://*bet88*/*
+// @match        *://*250*/*
+// @match        *://*bk8*/*
+// @match        *://*vn88*/*
+// @match        *://*fb88*/*
 
 // @exclude        https://yeumoney.com/quangly/check*
 // @grant        none
