2887f75f06cd07b064e4f792407e3b000adce3f7
Đại <dai26032007@gmail.com>
2025-07-23 00:13:20 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Bypass Yeumoney (Đại)
 // @namespace    http://tampermonkey.net/
-// @version      18/07/2025
+// @version      NBĐ-0001
 // @description  Auto vượt yeumoney không cần chạm tay
 // @author       Nguyễn Bá Đại
 // @require      https://raw.githubusercontent.com/dai26032007/Bypass-Yeumoney/refs/heads/main/bypass_yeumoney.js
+// @downloadURL      https://raw.githubusercontent.com/dai26032007/Bypass-Yeumoney/refs/heads/main/tampermonkey.user.js
 // @match        https://yeumoney.com/*
 // @exclude      https://yeumoney.com/quangly/*
 // @icon         https://i.imgur.com/a7HCsJr.jpeg
